96f75403383ed8dbc805fe3f7e6a5a90ee8d0180
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-10-04 11:40:46 -0400

    provider + project setup

---
+++ root/main.tf
@@ -0,0 +1,13 @@
+resource "google_project" "vpc_project" {
+  name                = var.project_name
+  project_id          = var.project_id
+  org_id              = var.org_id
+  billing_account     = var.billing_account_id
+  auto_create_network = false
+}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [google_project.vpc_project]
+
+  create_duration = "30s"
+}
