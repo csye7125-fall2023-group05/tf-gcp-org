7d33c22d5625565e6e382c0baecf70f3658403ec
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2023-10-27 16:22:36 -0400

    build: user data script to install kubectl & helm

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,3 +1,21 @@
+data "template_file" "startup_script" {
+  template = <<-EOF
+  sudo apt-get update -y
+  curl -LO https://dl.k8s.io/release/v1.28.3/bin/linux/amd64/kubectl
+  echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  chmod +x kubectl
+  mkdir -p ~/.local/bin
+  mv ./kubectl ~/.local/bin/kubectl
+  kubectl version --client
+
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+  chmod 700 get_helm.sh
+  ./get_helm.sh
+  helm version
+  EOF
+}
+
 resource "google_compute_instance" "vm" {
   name         = var.vm_name
   machine_type = var.machine_type
@@ -14,6 +32,8 @@ resource "google_compute_instance" "vm" {
   }
 
   tags = ["bastion"]
+
+  metadata_startup_script = data.template_file.startup_script.rendered
 
   network_interface {
     subnetwork = var.subnet_name
