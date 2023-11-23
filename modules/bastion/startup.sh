012defac5839b76bceee33190ac3a9f04922b53a
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-11-23 09:32:19 -0500

    fix: update startup script to install packages successfully
    Added IAM role binding to add the role "container admin" to the
    service account created and attached the cluster node pool.
    
    Fixes issue #30

--- modules/bastion/startup.sh
+++ modules/bastion/startup.sh
@@ -1,56 +1,19 @@
 #!/bin/bash
 
-# Kubectl Setup: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-echo "|                                                                                                                                         |"
-echo "|                                                             INSTALL KUBECTL                                                             |"
-echo "|                                                                                                                                         |"
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+sudo apt-get update -y
+curl -LO https://dl.k8s.io/release/v1.28.3/bin/linux/amd64/kubectl
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
-sudo apt-get update
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |
-  sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' |
-  sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update && sudo apt-get install kubectl -y
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version --client
 
-# Helm Setup: https://helm.sh/docs/intro/install/#from-apt-debianubuntu
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-echo "|                                                                                                                                         |"
-echo "|                                                              INSTALL HELM                                                               |"
-echo "|                                                                                                                                         |"
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm version
 
-sudo apt-get update
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
-sudo apt-get install apt-transport-https -y
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" |
-  sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update && sudo apt-get install helm -y
-
-# GCloud Setup: https://cloud.google.com/sdk/docs/install#deb
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-echo "|                                                                                                                                         |"
-echo "|                                                         INSTALL GCLOUD PLUGINS                                                          |"
-echo "|                                                                                                                                         |"
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates gnupg curl sudo -y
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-echo \
-  "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |
-  sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get update
-sudo apt-get install google-cloud-cli -y
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
-
-# Install tinyproxy for tunneling
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-echo "|                                                                                                                                         |"
-echo "|                                                            INSTALL TINYPROXY                                                            |"
-echo "|                                                                                                                                         |"
-echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
-
-sudo apt-get update && sudo apt-get install tinyproxy -y
+sudo apt-get update -y
+sudo apt-get install tinyproxy -y
