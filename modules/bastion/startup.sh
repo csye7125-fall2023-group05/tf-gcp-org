fa4c0e2068b1a16786fafb91c967a655ed27ff3b
Karan Wadhwa <wadhwa.karan96@gmail.com>
2023-12-01 02:59:30 -0500

    chore: update startup script

--- modules/bastion/startup.sh
+++ modules/bastion/startup.sh
@@ -16,4 +16,14 @@ chmod 700 get_helm.sh
 helm version
 
 sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates gnupg curl sudo -y
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo \
+  "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |
+  sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update
+sudo apt-get install google-cloud-cli -y
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
+
+sudo apt-get update -y
 sudo apt-get install tinyproxy -y
